2fa7988be415f30635b62b2243cf1df5c39c30ec
bhargavgunjan14-web <bhargavgunjan14@gmail.com>
2025-12-10 09:34:05 +0530

    Create patternI.js

---
+++ patternI.js
@@ -0,0 +1,18 @@
+let n = 7;
+
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+
+        if (
+            i === 0 ||                    
+            i === n - 1 ||               
+            j === Math.floor(n / 2)       
+        ) {
+            row += "* ";
+        } else {
+            row += "  ";
+        }
+    }
+    console.log(row);
+}
