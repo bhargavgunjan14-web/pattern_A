1a7ee18d987bafc3c4ecf1112b4042c115ae14f1
bhargavgunjan14-web <bhargavgunjan14@gmail.com>
2025-12-10 09:31:38 +0530

    Create pattern G.js

---
+++ pattern G.js
@@ -0,0 +1,29 @@
+let n = 7;
+
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+
+        if (
+            
+            (i === 0 && j > 0 && j < n - 1) ||
+
+            
+            (i === n - 1 && j > 0 && j < n - 1) ||
+
+          
+            (j === 0 && i > 0 && i < n - 1) ||
+
+          
+            (j === n - 1 && i >= Math.floor(n / 2) && i < n - 1) ||
+
+            
+            (i === Math.floor(n / 2) && j >= Math.floor(n / 2))
+        ) {
+            row += "* ";
+        } else {
+            row += "  ";
+        }
+    }
+    console.log(row);
+}
